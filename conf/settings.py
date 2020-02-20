28b3827697d18f5e351c9b5d73e87fcc14f1f1eb
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-02-20 13:43:08 -0500

    creates accounts app and tools app

--- conf/settings.py
+++ conf/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+
+    #loocal
+    'accounts.apps.AccountsConfig',
+    'tools.apps.ToolsConfig',
 ]
 
 MIDDLEWARE = [
