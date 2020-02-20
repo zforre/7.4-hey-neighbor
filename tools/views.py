4dedabfa11cadb5d3b1f328396848aad101fc2dc
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-02-20 16:10:23 -0500

    works to clear errors

--- tools/views.py
+++ tools/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Tool
 
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = 'tools/index.html'
+    model = Tool
