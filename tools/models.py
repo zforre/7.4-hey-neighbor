11f7c0185266717cd9d7b9fcdfe8f6a631fa694a
Zach Forrest <zachforre@gmail.com>
2020-02-28 11:12:56 -0500

    adds image field for tools

--- tools/models.py
+++ tools/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Tool(models.Model):
     name = models.CharField(max_length=50)
     owner = models.CharField(max_length=50, null=True)
+    photo = models.URLField(null=True)
 
     def __str__(self):
         return self.name
